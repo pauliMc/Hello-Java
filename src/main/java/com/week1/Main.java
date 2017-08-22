bb593fd052e55c1029d76d07de203542b2b66015
blaw blaw <blaw@gmail.com>
2017-08-22 11:57:07 -0500

    Price program

--- src/main/java/com/week1/Main.java
+++ src/main/java/com/week1/Main.java
@@ -9,10 +9,18 @@ public class Main {
     
     public static void main(String[] args) {
         
-        /* Write your code here */
+        double mms = doubleInput("How many M$Ms in a packet?");
+        int people = intInput("How many people like to share?");
         
         String name = stringInput("Please enter your name?");
         System.out.println("Hello, " + name + "!");
+
+        int students = intInput ("How many students?");
+        System.out.println("There are " + students + " students in class.");
+
+        double mmsEach = mms / people;
+
+        System.out.printf("Each person will get " + mmsEach + "M&Ms each.");
         
     }
     
---
+++ src/main/java/com/week1/Price.java
@@ -0,0 +1,21 @@
+package com.week1;
+
+/**
+ * Created by bw1795cr on 8/22/2017.
+ */
+
+import static input.InputUtils.*;
+
+public class Price {
+    public static void main(String[] args) {
+        String productName = stringInput("What is the name of the product?");
+        double price = doubleInput("What does " + productName + " cost?");
+        int quantity = intInput("How many " + productName + " to sell?");
+
+        //CAlculate the total price
+        double totalPrice = price + quantity;
+
+        System.out.println(quantity + " of " + productName + " at $" + price + "each costs $" + totalPrice);
+
+    }
+}
